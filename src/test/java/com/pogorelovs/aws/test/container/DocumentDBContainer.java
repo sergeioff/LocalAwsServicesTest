666477532bd739ee511ab14a04548fde3a9c4ea2
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2020-06-29 15:38:39 +0300

    test(S3+DocumentDB): add test examples for AWS S3 and DocumentDB

---
+++ src/test/java/com/pogorelovs/aws/test/container/DocumentDBContainer.java
@@ -0,0 +1,6 @@
+package com.pogorelovs.aws.test.container;
+
+import org.testcontainers.containers.GenericContainer;
+
+public class DocumentDBContainer extends GenericContainer<DocumentDBContainer> {
+}
