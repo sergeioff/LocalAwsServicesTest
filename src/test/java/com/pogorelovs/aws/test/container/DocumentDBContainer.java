2189afde225bc21566fe8330bdd33a9fed0b97bf
Sergey Pogorelov <pogorelov.s.y@gmail.com>
2020-06-29 15:39:57 +0300

    del(container): remove redundant class

--- src/test/java/com/pogorelovs/aws/test/container/DocumentDBContainer.java
+++
@@ -1,6 +0,0 @@
-package com.pogorelovs.aws.test.container;
-
-import org.testcontainers.containers.GenericContainer;
-
-public class DocumentDBContainer extends GenericContainer<DocumentDBContainer> {
-}
